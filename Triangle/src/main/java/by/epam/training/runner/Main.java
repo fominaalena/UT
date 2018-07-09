1ef9d1b5d61bc7b3968a5fc4b821214e99d7b0f5
Alena Fomina <32820331+fominaalena@users.noreply.github.com>
2018-07-09 22:50:02 +0300

    Update Main.java

--- Triangle/src/main/java/by/epam/training/runner/Main.java
+++ Triangle/src/main/java/by/epam/training/runner/Main.java
@@ -5,7 +5,7 @@ import by.epam.training.triangle.Triangle;
 public class Main {
 
 	public static void main(String[] args) {
-		Triangle tr = new Triangle(1, 2, 3);
+		Triangle tr = new Triangle(1.0, 2.0, 3.0);
 		System.out.println(tr.checkTriangle());
 		System.out.println(tr.getMessage());
 		System.out.println(tr.detectTriangle());
